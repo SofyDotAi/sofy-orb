297f8c5bfad4136cfe94483d9b502a31d06829b1
SofyDotAi <59080606+SofyDotAi@users.noreply.github.com>
2021-07-12 00:36:46 -0500

    Create connect_script.sh

---
+++ src/scripts/connect_script.sh
@@ -0,0 +1,5 @@
+connect() {
+  curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+  --header 'SubscriptionKey: ${SUBS_KEY}' \
+  --form 'applicationFile=@"${APK_PATH}"'
+}
