33ceec7b5d3020aecd86bb6474a9cb429a8c9f01
SofyDotAi <59080606+SofyDotAi@users.noreply.github.com>
2021-07-13 12:59:45 -0500

    [semver:skip] (#21)
    * Update connect_sofy.yml
    
    * Create connect_script.sh
    
    * Update connect_sofy.yml
    
    * Update connect_sofy.yml
    
    * Update connect_sofy.yml
    
    * Update connect_sofy.yml
    
    * Update connect_sofy.yml
    
    * Update connect_script.sh

--- src/scripts/connect_script.sh
+++ src/scripts/connect_script.sh
@@ -1,5 +1,10 @@
 connect() {
-  curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
-  --header 'SubscriptionKey: ${SUBS_KEY}' \
-  --form 'applicationFile=@"${APK_PATH}"'
+  echo ${SUBS_KEY}
+  echo ${APK_PATH}
+  x = $(curl --location --request POST 'https://api-sofy-test.azurewebsites.net/api/AppTests/buildUpload' \
+  --header "SubscriptionKey: ${SUBS_KEY}" \
+  --form "applicationFile=@"${APK_PATH}"")
+  echo ${x}
 }
+connect()
+
